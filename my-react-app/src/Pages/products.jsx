2342d1287be30caac7770aad4db9daefeae17e99
unknown <dhymazaldhyz2@gmail.com>
2024-03-17 11:33:53 +0700

    lifecycle and useEffect

--- my-react-app/src/Pages/products.jsx
+++ my-react-app/src/Pages/products.jsx
@@ -1,6 +1,7 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
+import Counter from "../components/Fragments/Counter"
 
 const products = [
     {
@@ -46,12 +47,22 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductsPage = () => {
-    const [cart, setCart] = useState([
-        {
-            id: 1,
-            qty: 1,
-        },
-    ]);
+    const [cart, setCart] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect(()=>{
+        setCart(JSON.parse(localStorage.getItem('cart')) || []);
+    },[])
+
+    useEffect(()=>{
+       if(cart.length > 0){
+        const sum = cart.reduce((acc,item)=>{
+            const product = products.find((product)=>product.id === item.id);
+            return acc + product.price * item.qty;
+        }, 0);
+        setTotalPrice(sum);
+        localStorage.setItem("cart", JSON.stringify(cart))
+       }
+    }, [cart])
 
     const handleLogout = () => {
         localStorage.removeItem("email");
@@ -102,7 +113,7 @@ const ProductsPage = () => {
                 </div>
                 <div className="w-1/3">
                     <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
-                    <table className="text-left table-auto border-separate border border-slate-400 border-spacing-x-5">
+                    <table className="text-left table-auto border-separate border border-slate-400 border-spacing-y-5 border-spacing-x-2">
                         <thead>
                             <tr>
                                 <th>Product</th>
@@ -127,10 +138,17 @@ const ProductsPage = () => {
                                     </tr>
                                 );
                             })}
+                            <tr>
+                                <td colSpan={3}><b>Total Price</b></td>
+                                <td><b>Rp {totalPrice.toLocaleString('id-ID')}</b></td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
+            {/* <div className="my-5 flex justify-center">
+                <Counter></Counter>
+            </div> */}
         </Fragment>
     );
 };
